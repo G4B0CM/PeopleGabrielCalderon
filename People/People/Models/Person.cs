2e4f38f7dd6d28e6e1200cfe812fffa0e55b96f0
G4B0CM <Gabriel.calderon.matute@udla.edu.ec>
2024-12-22 18:52:47 -0500

    Modelo Person creado

---
+++ People/People/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace People.Models
+{
+    [SQLite.Table("People")]
+    public class Person
+    {
+        [PrimaryKey,AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(100),Unique]
+        public string Name { get; set; }
+    }
+}
